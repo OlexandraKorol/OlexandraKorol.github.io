200f680fe6efe34e2e7435e37508444ccc1b4f08
Olexandra Korol <korol.olexandra@gmail.com>
2024-04-18 01:13:41 +0300

    first version of ui todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoList} from './components/TodoList';
+import styled from '@emotion/styled';
+
+const test = [1,2,3,4,5]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <TodoList />
+    </Wrapper>
+    )
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  background-color: green;
+  height: max-content;
+  padding: 40px
+`;
 
 export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import Paper from '@mui/material/Paper';
+
+
+export const TodoList = () => {
+  const [checked, setChecked] = React.useState<readonly number[]>([]);
+  const [listItems] = React.useState<readonly number[]>([0, 1, 2, 3]);
+
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const customList = (items: readonly number[]) => (
+    <Paper sx={{ width: 200, height: 230, overflow: 'auto' }}>
+      <List dense component="div" role="list">
+        {items.map((value: number) => {
+          const labelId = `transfer-list-item-${value}-label`;
+
+          return (
+            <ListItemButton
+              key={value}
+              role="listitem"
+              onClick={handleToggle(value)}
+            >
+              <ListItemIcon>
+                <Checkbox
+                  checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{
+                    'aria-labelledby': labelId,
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`List item ${value + 1}`} />
+            </ListItemButton>
+          );
+        })}
+      </List>
+    </Paper>
+  );
+
+  return (
+    <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <Grid item>{customList(listItems)}</Grid>
+    </Grid>
+  );
+}
+
+
